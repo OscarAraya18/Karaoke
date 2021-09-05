b8e7d79ff2dd7fc6d6abd85375f1d8001b0959fa
Oscar Araya <osaronaragar15@gmail.com>
2021-09-05 09:58:40 -0600

    [CAMBIOS @app.routes.ts] Agrego los nuevos componentes a la ruta del Router del navegador

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { PlayerComponent } from './componentes/player/player.component';
+import { EditarPopupComponent } from './componentes/editar-popup/editar-popup.component';
+import { SiNoPopupComponent } from './componentes/si-no-popup/si-no-popup.component';
+
 
 const routes: Routes = [
   { path: '', component:LoginComponent},
-  { path: 'home', component:HomeComponent}
-
+  { path: 'home', component:HomeComponent},
+  { path: 'player', component:PlayerComponent},
+  { path: 'editarCrear', component:EditarPopupComponent},
+  { path: 'siNo', component:SiNoPopupComponent}
 ];
 
 @NgModule({
